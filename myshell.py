82ded28d10f21e9c24d4cc6c1b6e36b52f768d81
Josh Grant <Josh_grant1@baylor.edu>
2021-11-17 15:19:56 -0600

    initial commit

---
+++ myshell.py
@@ -0,0 +1,24 @@
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE","learning_log.settings")
+
+import django
+django.setup()
+
+from MainApp.models import Topic, Entry
+
+topics = Topic.objects.all()
+
+for topic in topics:
+    print(topic.id, topic)
+
+
+t = Topic.objects.get(id=1)
+
+print(t.text)
+print(t.date_added)
+
+entries = t.entry_set.all()
+
+for entry in entries:
+    print(entry)
+
