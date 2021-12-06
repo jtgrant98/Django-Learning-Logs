4cd7c3b34a5d5531d551474556800e2d35a7dd76
Josh Grant <Josh_grant1@baylor.edu>
2021-12-06 15:11:37 -0600

    committing all that we have done in class so far

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,3 +1,67 @@
-from django.shortcuts import render
+from django.forms.forms import Form
+from django.http import request
+from django.shortcuts import redirect, render
+from .forms import TopicForm, EntryForm
+from .models import Topic
 
 # Create your views here.
+def index(request):
+    return render(request, 'MainApp/index.html')
+
+def topics(request):
+    topics = Topic.objects.all().order_by('date_added')
+
+    context = {'topics':topics}
+
+    return render(request, 'MainApp/topics.html', context)
+
+def topic(request,topic_id):
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topic':topic, 'entries':entries}
+
+    return render(request, 'MainApp/topic.html', context)
+
+def new_topic(request):
+    if request.method != 'POST':
+        form = TopicForm()
+
+    else:
+        form = TopicForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+
+            return redirect('MainApp:topics')
+    context = {'form' :form}
+    return render(request, 'MainApp/new_topic.html', context)
+
+def new_entry(request, topic_id):
+    topic = Topic.objects.get(id=topic_id)
+    if request.method != 'POST':
+        form = EntryForm()
+    else:
+        form = EntryForm(data=request.POST)
+
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return redirect('MainApp:topic',topic_id=topic_id)
+
+    context = {'form':form, 'topic':topic}
+    return render(request, 'MainApp/new_entry.html',context )
+
+def edit_entry(request, entry_id):
+    entry = Entry.object.get(id=entry_id)
+    topic = entry.topic
+
+    if request.method != 'POST':
+        form - EntryForm(instance = entry)
+    else:
+        form = EntryForm(instance = entry, data=request.POST)
+        if form.isvalid():
+            form.save()
+            return redirect('MainApp:topic', topic_id=topic.id)
+    
+    context = {'entry':entry, 'topic':topic, 'form':form}
+    return render(request, 'MainApp/edit_entry.html',context)
